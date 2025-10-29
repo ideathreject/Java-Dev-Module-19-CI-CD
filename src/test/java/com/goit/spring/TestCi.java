f79cb3a695c729a752fb8908e0102d1c2cfe2f3c
ideathreject <fury.extreme1@gmail.com>
2025-10-29 12:33:34 +0100

    test ci

---
+++ src/test/java/com/goit/spring/TestCi.java
@@ -0,0 +1,11 @@
+package com.goit.spring;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class TestCi {
+    @Test
+    void twoPlusTwo() {
+        assertEquals(4, 2 + 2);
+    }
+}
